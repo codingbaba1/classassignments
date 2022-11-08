f6573f3b4b5cfad43b6de40eb930f9a627b55d5f
Mohd Imran Khan <79039055+codingbaba1@users.noreply.github.com>
2022-11-08 15:28:11 +0530

    Add files via upload

---
+++ oopsconcept/Demo.java
@@ -0,0 +1,19 @@
+package oops_concept;
+
+class Student 
+{
+	int id;
+	String name,address,course;
+}
+public class Demo{
+	
+
+	public static void main(String[]arg)
+	{
+		Student imran=new Student();
+		System.out.println(imran);// address of memory
+		
+		
+		
+	}
+}
